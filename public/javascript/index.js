d1711abf62f6ce464dd99e95274dfb8c73aa6a00
1Cbondy1 <claytonbondy@gmail.com>
2019-04-10 00:07:31 -0500

    built modal and added data-targets

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -14,7 +14,7 @@ $( document ).ready(function() {
 
 });
 
-var queryURL = "https://developer.nps.gov/api/v1/parks?limit=50&start=50&q=National%20Park&fields=images&sort=-designation&api_key=yflsYenzvnQI9KvZVIiYrgee1zhKUiglWNoaPfa2"
+var queryURL = "https://developer.nps.gov/api/v1/parks?limit=50&start=50&q=National%20Park&fields=images,entranceFees&sort=-designation&api_key=yflsYenzvnQI9KvZVIiYrgee1zhKUiglWNoaPfa2"
 
 $.ajax({
     url: queryURL,
@@ -27,6 +27,19 @@ $.ajax({
         var parkName = response.data[i].fullName;
         var parkDes = response.data[i].description;
         var parkImg = response.data[i].images[0].url;
+        var parkState = response.data[i].states;
+
+        // var parkCost;
+
+        // if (response.data[i].entranceFees == null || response.data[i].entranceFees == undefined || response.data[i].entranceFees == "undefined") {
+        //     parkCost = "No cost information exists.";
+        //     return;
+        // } else if (response.data[i].entranceFees[1].cost == null || response.data[i].entranceFees == undefined || response.data[i].entranceFees == "undefined") {
+        //     parkCost = "No cost information exists.";
+        //     return;
+        // } else {
+        //     parkCost = response.data[i].entranceFees[1].cost;
+        // }
 
         var parkCard = 
         $("<span class='card' data-id='" + i + "' style='width: 18rem;'>" +
@@ -48,11 +61,31 @@ $.ajax({
             "</span>" +
 
             // Button trigger modal
-            "<button type='button' class='btn btn-success' data-toggle='modal' data-target='#exampleModal'>See more...</button>" +
+            "<button type='button' class='btn btn-success' data-toggle='modal' data-target='#exampleModal-" + i + "'>See more...</button>" +
         "</span>");
+
+        var modalDiv =
+        $("<div class='modal fade' id='exampleModal-" + i + "' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>" +
+            "<div class='modal-dialog' role='document'>" +
+                "<div class='modal-content'>" +
+                    "<div class='modal-header'>" +
+                        "<h5 class='modal-title' id='exampleModalLabel'>" + parkName + "</h5>" +
+                        "<button type='button' class='close' data-dismiss='modal' aria-label='Close'>" +
+                        "<span aria-hidden='true'>&times;</span>" +
+                        "</button>" +
+                    "</div>" +
+                    "<div class='modal-body card-text' id='modal-des'>Description: " + parkDes + "</div>" +
+                    "<div class='modal-body card-text' id='modal-des'>State: " + parkState + "</div>" +
+                    "<div class='modal-footer'>" +
+                        "<button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>" +
+                    "</div>" +
+                "</div>" +
+            "</div>" +
+        "</div>");
 
         parkCardSpan.append(parkCard);
         $("#park-card").append(parkCardSpan);
+        $("#modal-div").append(modalDiv);
     }
 
     // updates check-num value after ajax request
